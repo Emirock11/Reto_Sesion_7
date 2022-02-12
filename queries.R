84c62584ceb9902b2290942151a7ab8c678f9f7a
Emirock11 <emi.rock11@hotmail.com>
2022-02-12 22:37:18 +0000

    Reto terminado

--- queries.R
+++ queries.R
@@ -4,3 +4,37 @@ gitcreds_set()
 
 # git config --global user.name "Emirock11"
 # git config --global user.email "emi.rock11@hotmail.com"
+
+# install.packages("DBI")
+# install.packages("RMySQL")
+# install.packages("dplyr")
+
+library(DBI)
+library(RMySQL)
+library(dplyr)
+
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'CountryLanguage')
+
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+
+popEsp <-  DataDB %>% filter(Language == "Spanish")
+
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(popEsp, aes(x = Percentage, y = CountryCode, col = IsOfficial)) + geom_point() 
+
+
+
+
+  
